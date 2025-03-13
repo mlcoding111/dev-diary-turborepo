16d7607f02e1a2de63261f51da266d8597f4c330
mlcoding111 <kennycodes@hotmail.com>
2025-03-13 16:05:46 -0400

    feat(dev-diary-api): implement product creation and retrieval, add types for product requests

--- apps/dev-diary-api/src/products/products.controller.ts
+++ apps/dev-diary-api/src/products/products.controller.ts
@@ -1,10 +1,19 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { ProductsService } from './products.service';
+import { type CreateProductRequest } from '@repo/types';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
-  async createProduct(@Body() createProductRequest: any) {}
+  createProduct(@Body() createProductRequest: CreateProductRequest) {
+    console.log(createProductRequest);
+    return this.productsService.createProduct(createProductRequest);
+  }
+
+  @Get()
+  getProducts() {
+    return this.productsService.getProducts();
+  }
 }
