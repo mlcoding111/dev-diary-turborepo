40df32b946e881827c50aac8eb2545905fd8b234
mlcoding111 <kennycodes@hotmail.com>
2025-03-13 18:27:12 -0400

    feat(web): add @repo/types dependency and enhance Home component with CreateProduct and Products

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,12 @@
-import styles from "./page.module.css";
+import styles from './page.module.css';
+import CreateProduct from './create-product/create-product';
+import Products from './products/products';
 
 export default function Home() {
   return (
     <div className={styles.page}>
-     
+      <CreateProduct />
+      <Products />
     </div>
   );
 }
