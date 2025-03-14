1ed0793e5139efcc00ebaed33049ce42965bf631
mlcoding111 <kennycodes@hotmail.com>
2025-03-13 20:11:25 -0400

    add force-dynamic to fix pre-render issue

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,6 +2,8 @@ import styles from './page.module.css';
 import CreateProduct from './create-product/create-product';
 import Products from './products/products';
 
+export const dynamic = "force-dynamic";
+
 export default function Home() {
   return (
     <div className={styles.page}>
