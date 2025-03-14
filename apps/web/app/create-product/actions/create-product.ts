d3650e806f5ed8ee02415be02763e024146c9dc8
mlcoding111 <kennycodes@hotmail.com>
2025-03-13 21:27:59 -0400

    refactor(create-product): comment out fetch request in createProduct function

--- apps/web/app/create-product/actions/create-product.ts
+++ apps/web/app/create-product/actions/create-product.ts
@@ -2,13 +2,13 @@
 import { revalidateTag } from "next/cache";
 
 export default async function createProduct(formData: FormData) {
-	const response = await fetch(`${process.env.API_URL}/products`, {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify(Object.fromEntries(formData)),
-	});
+	// await fetch(`${process.env.API_URL}/products`, {
+	// 	method: "POST",
+	// 	headers: {
+	// 		"Content-Type": "application/json",
+	// 	},
+	// 	body: JSON.stringify(Object.fromEntries(formData)),
+	// });
 
 	revalidateTag("products");
 }
