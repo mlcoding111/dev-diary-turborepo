de86b49dbbdc79e60a8e2eebf6939fd54dcc6b72
mlcoding111 <kennycodes@hotmail.com>
2025-03-26 19:12:18 -0400

    refactor(repository): simplify BaseRepository by removing IBaseRepository interface and unnecessary class extension

--- apps/dev-diary-api/src/core/utils/repository/base.repository.ts
+++ apps/dev-diary-api/src/core/utils/repository/base.repository.ts
@@ -1,14 +1,8 @@
 import { Repository, ObjectLiteral } from 'typeorm';
 import { Injectable } from '@nestjs/common';
 
-export interface IBaseRepository<T extends ObjectLiteral>
-  extends Repository<T> {}
-
 @Injectable()
-export class BaseRepository<T extends ObjectLiteral>
-  extends Repository<T>
-  implements IBaseRepository<T>
-{
+export class BaseRepository<T extends ObjectLiteral> extends Repository<T> {
   constructor(private readonly repository: Repository<T>) {
     super(repository.target, repository.manager, repository.queryRunner);
   }
