8f14fbadddc0eba190253822e4159046352d5674
mlcoding111 <kennycodes@hotmail.com>
2025-03-26 15:21:49 -0400

    cleanup

--- apps/dev-diary-api/src/products/product.controller.ts
+++ apps/dev-diary-api/src/products/product.controller.ts
@@ -1,22 +1,12 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  HttpException,
-  HttpStatus,
-  InternalServerErrorException,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { Validate } from 'src/decorators/validation.decorator';
 import {
   type TCreateProduct,
   createProductSchema,
-  productSchema,
   productSchemaSerialized,
 } from '@repo/types/schema';
-import type { TProduct, TSerializedProduct } from '@repo/types/schema';
-import { ApiException } from 'src/core/utils/api/exception/ApiError.exception';
+import type { TSerializedProduct } from '@repo/types/schema';
 import z from 'zod';
 @Controller('products')
 export class ProductsController {
@@ -33,15 +23,11 @@ export class ProductsController {
   }
 
   @Validate({
-    // This is giving error: unsafe assigment of an error typed value
     output: z.array(productSchemaSerialized),
-
-    // But somehow this is working:
-    // output: productSchema,
   })
   @Get()
   getProducts(): TSerializedProduct[] {
-    // TODO: Replace with serialized
+    // TODO: Replace with serializer
     const serializedProducts = this.productsService
       .getProducts()
       .map((product: TSerializedProduct) => ({
@@ -51,53 +37,5 @@ export class ProductsController {
       }));
 
     return serializedProducts;
-
-    // Errors examples:
-
-    throw new Error('User Not Found');
-
-    // throw new HttpException('User Not Found', HttpStatus.NOT_FOUND);
-
-    // throw new ApiException({
-    //   status_code: HttpStatus.NOT_FOUND,
-    //   error_code: 'USER_NOT_FOUND',
-    //   // Optional
-    //   message: 'User Not Found',
-    //   data: {
-    //     test: 'test',
-    //   },
-    // });
   }
-  // @Validate({
-  //   output: productSchema,
-  // })
-  // @Get()
-  // getProducts(): TSerializedProduct[] {
-  //   // throw new Error('This is a test');
-
-  //   // throw new InternalServerError({
-  //   //   metadata: {
-  //   //     test: 'test',
-  //   //   },
-  //   // });
-
-  //   // throw new ApiException({
-  //   //   message: 'test',
-  //   //   error_code: 'TEST_ERROR',
-  //   //   status_code: 500,
-  //   //   data: null,
-  //   // });
-
-  //   // This would throw and error if the output is not matching the expected schema.
-  //   // const transformedProducts = this.productsService
-  //   //   .getProducts()
-  //   //   .map((product) => ({
-  //   //     id: product.id,
-  //   //     name: product.name,
-  //   //     title: product.title,
-  //   //     test: 'yop',
-  //   //   }));
-  //   // return transformedProducts;
-  //   return this.productsService.getProducts();
-  // }
 }
