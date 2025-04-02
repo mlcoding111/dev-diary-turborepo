c0349cf8033fa648406463295659d40357b20947
mlcoding111 <kennycodes@hotmail.com>
2025-04-01 23:40:46 -0400

    Add lodash dependency and update pagination handling
    - Added lodash as a dependency in package.json.
    - Introduced PaginationResult in base.service.ts for improved pagination handling.
    - Updated TransformInterceptor to accommodate PaginationResult and handle various data types.
    - Modified TApiResponse type to allow data to be an array or null.

--- apps/dev-diary-api/src/interceptors/transform.interceptor.ts
+++ apps/dev-diary-api/src/interceptors/transform.interceptor.ts
@@ -7,20 +7,45 @@ import {
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { TApiResponseSuccess } from '@repo/types/api';
-import { PaginatedResult } from '@/core/utils/service/base.service';
+import { PaginationResult } from '@/core/utils/service/pagination';
+import { Reflector } from '@nestjs/core';
+import { omit } from 'lodash';
 
 @Injectable()
 export class TransformInterceptor<T>
-  implements NestInterceptor<T, TApiResponseSuccess<T>>
+  implements NestInterceptor<T, TApiResponseSuccess<any>>
 {
+  constructor(private readonly reflector: Reflector) {}
   intercept(
     context: ExecutionContext,
     next: CallHandler<T>,
-  ): Observable<TApiResponseSuccess<T>> {
+  ): Observable<TApiResponseSuccess<any>> {
+    // const isPaginated = this.reflector.get<boolean>(
+    //   'pagination',
+    //   context.getHandler(),
+    // );
+    // get reflection
     return next.handle().pipe(map((data) => this.formatResponse(data)));
   }
 
-  private formatResponse(data: T): TApiResponseSuccess<T> {
+  private formatResponse(
+    data: T | T[] | PaginationResult<T>,
+  ): TApiResponseSuccess<any> {
+    // Check if data has the structure of a PaginationResult
+    if (
+      data &&
+      typeof data === 'object' &&
+      'metadata' in data &&
+      data.metadata &&
+      '_isPaginated' in data.metadata
+    ) {
+      return {
+        success: true,
+        data: data.data,
+        message: 'success',
+        metadata: omit(data.metadata, '_isPaginated'),
+      };
+    }
     return {
       success: true,
       data: data,
