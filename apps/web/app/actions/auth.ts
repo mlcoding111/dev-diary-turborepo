08f43e3a064b133c4e5eabd0d9e2b66849e63e6e
mlcoding111 <kennycodes@hotmail.com>
2025-04-13 14:32:04 -0400

    implement session creation inside signup action

--- apps/web/app/actions/auth.ts
+++ apps/web/app/actions/auth.ts
@@ -5,21 +5,26 @@ import { TRegisterUser } from "@repo/types/schema";
 import { revalidateTag } from "next/cache";
 import { TApiResponse } from "@repo/types/api";
 import { TSerializedMe } from "@repo/types/schema";
+import { createSession } from "@/lib/session";
 
 export async function signup(
 	data: TRegisterUser,
 ): Promise<TApiResponse<TSerializedMe>> {
-	const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/register`, {
+	const response: TApiResponse<TSerializedMe> = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/register`, {
 		method: "POST",
 		body: JSON.stringify(data),
 		headers: {
 			"Content-Type": "application/json",
 		},
-	});
+	}).then(res => res.json());
 
-	if (response.ok) {
+	if (response.success) {
 		revalidateTag("me");
 	}
 
-	return response.json();
+	const user = response.data?.user;
+
+	await createSession(user.id);
+
+	return response;
 }
