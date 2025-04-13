e338adccb5a5b689ebe9b011c9af70b79f7e43f9
mlcoding111 <kennycodes@hotmail.com>
2025-04-13 14:28:49 -0400

    setup session utils functions

--- apps/web/app/(public)/signup/form.tsx
+++ apps/web/app/(public)/signup/form.tsx
@@ -5,83 +5,84 @@ import { signup } from "@/app/actions/auth";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { TRegisterUser, registerUserSchema } from "@repo/types/schema";
-import { useForm } from 'react-hook-form'
+import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import Form from "@/components/common/Form";
 import { FormField, FormItem, FormLabel, FormControl, FormMessage } from "@/components/ui/form";
 
 export function SignupForm() {
-  const form = useForm<TRegisterUser>({
-    resolver: zodResolver(registerUserSchema),
-    defaultValues: {
-      email: '',
-      password: '',
-      first_name: '',
-      last_name: '',
-    },	
-  });
+	const form = useForm<TRegisterUser>({
+		resolver: zodResolver(registerUserSchema),
+		defaultValues: {
+			email: "",
+			password: "",
+			first_name: "",
+			last_name: "",
+		},
+	});
 
-  const onSubmit = async (data: TRegisterUser) => {
-    return await signup(data);
-  };
+	const onSubmit = async (data: TRegisterUser) => {
+		return await signup(data);
+	};
 
-  return (
-    <Form form={form} onSubmit={onSubmit}>
-        <FormField 
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField 
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Password</FormLabel>
-              <FormControl>
-                <Input type="password" {...field} />
-              </FormControl>
-              <FormMessage />	
-            </FormItem>
-          )}
-        />
-        <FormField 
-          control={form.control}
-          name="first_name"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>First Name</FormLabel>
-              <FormControl>
-                <Input {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField 
-          control={form.control}
-          name="last_name"	
-          render={({ field }) => (	
-            <FormItem>
-              <FormLabel>Last Name</FormLabel>
-              <FormControl>
-                <Input {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>		
-          )}
-        />
-		{/* Get the api response and display response.message */}
+	return (
+		<Form form={form} onSubmit={onSubmit}>
+			<FormField
+				control={form.control}
+				name="email"
+				render={({ field }) => (
+					<FormItem>
+						<FormLabel>Email</FormLabel>
+						<FormControl>
+							<Input {...field} />
+						</FormControl>
+						<FormMessage />
+					</FormItem>
+				)}
+			/>
+			<FormField
+				control={form.control}
+				name="password"
+				render={({ field }) => (
+					<FormItem>
+						<FormLabel>Password</FormLabel>
+						<FormControl>
+							<Input type="password" {...field} />
+						</FormControl>
+						<FormMessage />
+					</FormItem>
+				)}
+			/>
+			<FormField
+				control={form.control}
+				name="first_name"
+				render={({ field }) => (
+					<FormItem>
+						<FormLabel>First Name</FormLabel>
+						<FormControl>
+							<Input {...field} />
+						</FormControl>
+						<FormMessage />
+					</FormItem>
+				)}
+			/>
+			<FormField
+				control={form.control}
+				name="last_name"
+				render={({ field }) => (
+					<FormItem>
+						<FormLabel>Last Name</FormLabel>
+						<FormControl>
+							<Input {...field} />
+						</FormControl>
+						<FormMessage />
+					</FormItem>
+				)}
+			/>
+			{/* Get the api response and display response.message */}
 
-        <Button type="submit">Sign Up</Button>
-    </Form>
-  );
+			<Button type="submit">Sign Up</Button>
+			<p>Pending {form.formState.isSubmitting ? "true" : "false"}</p>
+		</Form>
+	);
 }
---
+++ apps/web/lib/session.ts
@@ -0,0 +1,61 @@
+import 'server-only';
+import { JWTPayload, SignJWT, jwtVerify } from "jose";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+const key = new TextEncoder().encode(process.env.NEXT_PUBLIC_JWT_SECRET);
+
+const cookie = {
+    name: "session",
+    options: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "lax",
+        // maxAge: 60 * 60 * 24 * 30,
+        path: "/",
+    },
+    duration: 24 * 60 * 60 * 1000,
+}
+export async function encrypt(payload: JWTPayload){
+    return await new SignJWT(payload)
+    .setProtectedHeader({ alg: "HS256" })
+    .setIssuedAt()
+    .setExpirationTime('1day')
+    .sign(key);
+}
+
+export async function decrypt(session: string){
+    try{
+        const { payload } = await jwtVerify(session, key, {
+            algorithms: ["HS256"],
+        });
+        return payload;
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+}
+
+export async function createSession(userId: string){
+    const expires = new Date(Date.now() + cookie.duration);
+    const session = await encrypt({ userId, expires});
+    const cookieStore = await cookies();
+    
+    cookies().set(cookie.name, session, cookie.options);
+    redirect("/dashboard")
+}
+
+export async function verifySession(){
+    const cookie = (await cookies()).get(cookies.name)?.value;
+    const session = await decrypt(cookie);
+    if(!session?.userId) {
+        redirect("/login");
+    }
+    return { userId: session.userId };
+}
+
+export async function deleteSession(){
+    cookies().delete(cookie.name);
+
+    redirect("/login");
+}
