16842ba71f3a37c31f51c6080303c439f15c5fac
mlcoding111 <kennycodes@hotmail.com>
2025-05-21 17:37:11 -0400

    minor change

--- apps/web/app/dashboard/page.tsx
+++ apps/web/app/dashboard/page.tsx
@@ -10,7 +10,7 @@ export default async function Dashboard(){
     //     return (
     //         <div>
     //             <h1>Dashboard</h1>
-    //             <p>{user?.email}</p>
+    //             <p>{user?.email}</p
     //         </div>
     //     )
     // }
