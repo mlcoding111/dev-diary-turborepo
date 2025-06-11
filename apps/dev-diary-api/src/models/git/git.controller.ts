5d52cfe0c5afb9ad5153f14be92132b8f82b71c9
mlcoding111 <kennycodes@hotmail.com>
2025-06-11 16:15:54 -0400

    minor refactor

--- apps/dev-diary-api/src/models/git/git.controller.ts
+++ apps/dev-diary-api/src/models/git/git.controller.ts
@@ -1,25 +1,11 @@
 import {
   Controller,
   Get,
-  Post,
-  Body,
-  Param,
-  Put,
-  Delete,
-  Query,
   UseInterceptors,
   ClassSerializerInterceptor,
-  NotFoundException,
 } from '@nestjs/common';
 import { GitService } from './git.service';
 import { Validate } from 'src/decorators/validation.decorator';
-import { z } from 'zod';
-import {
-  PaginatedResult,
-  type PaginationOptions,
-} from '@/core/utils/service/base.service';
-import { GitResolverService } from '@/modules/git/git-resolver.service';
-import { GitProviderType } from '@repo/types/integrations';
 
 @Controller('git')
 @UseInterceptors(ClassSerializerInterceptor)
@@ -30,15 +16,21 @@ export class GitController {
     bypass: true,
   })
   @Get('commits')
-  async findAll(
-    @Query() query: PaginationOptions,
-  ): Promise<PaginatedResult<any>> {
+  async findAll(): Promise<any> {
     const gitProvider = await this.gitService.resolveProvider();
-    // const userProfile = await gitProvider.getUserProfile();
     const commits = await gitProvider.getCommits('my-turborepo');
 
     console.log('The commits are', commits);
     return commits;
-    return await this.gitService.paginate(query);
+  }
+
+  @Validate({
+    bypass: true,
+  })
+  @Get('repositories')
+  async getRepositories(): Promise<any> {
+    const gitProvider = await this.gitService.resolveProvider();
+    const repositories = await gitProvider.getRepositories();
+    return repositories;
   }
 }
